28bc8e27eca3b876bc634a21e2772cc9e49753eb
M-E-C-20 <mcasey8511@gmail.com>
2021-05-04 14:51:26 +0000

    Create add_exercise template

--- app.py
+++ app.py
@@ -104,6 +104,11 @@ def logout():
     return redirect(url_for("login"))
 
 
+@app.route("/add_exercise")
+def add_exercise():
+    return render_template("add_exercise.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
