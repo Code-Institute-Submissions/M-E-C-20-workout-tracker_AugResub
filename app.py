b94101e490aa6313075f338f11b8848c6ace8950
M-E-C-20 <mcasey8511@gmail.com>
2021-08-09 15:05:02 +0000

    Changed debug to false

--- app.py
+++ app.py
@@ -322,4 +322,4 @@ def delete_exercise_db(exercise_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
